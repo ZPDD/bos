1246b7cfed1373a0ca5831db580ceb70ae222b7f
David Borsato <77537362+ZPDD@users.noreply.github.com>
2021-05-21 09:54:36 -0400

    Update q.sh

--- QEMU/q.sh
+++ QEMU/q.sh
@@ -4,10 +4,10 @@
 #
 # May 21, 2021 - David Borsato.
 
-# NOTE: The last line is commented out. Use this if you want to enable GDB or
+# NOTE: The last line is commented out. Uncomment this line if you want to enable GDB or
 #       LLDB (on MAC) to debug.
 
- qemu-system-x86_64 -drive format=vpc,file=ht3.vhd -d cpu_reset -monitor stdio \
+ qemu-system-x86_64 -drive format=vpc,file=bos.vhd -d cpu_reset -monitor stdio \
   -device e1000,netdev=net0, -netdev user,id=net0,hostfwd=tcp::80-:80 \
   -device sb16 -audiodev coreaudio,id=coreaudio,out.frequency=48000,out.channels=2,out.format=s32 \
   # -s -S
